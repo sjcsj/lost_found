78adb497fa1c2c6094aa704dd98c8f9497460e12
Trust <2965404503@qq.com>
2024-12-22 18:28:17 +0800

    初始化项目

---
+++ src/test/java/com/demo/lost_found/LostFoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.lost_found;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostFoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
